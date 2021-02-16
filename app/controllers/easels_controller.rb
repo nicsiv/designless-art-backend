f9cf14a788cdb62f70da4fe4e43b4a9cad6c684a
Evan <evanfujita@gmail.com>
2021-02-16 17:24:41 -0600

    fix api

--- app/controllers/easels_controller.rb
+++ app/controllers/easels_controller.rb
@@ -1,3 +1,11 @@
 class EaselsController < ApplicationController
-   
+   before_action :require_login
+
+
+
+   private
+
+   def require_login
+    return head(:forbidden) unless session.include? :user_id
+   end
 end
