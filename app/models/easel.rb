b4b3195dfde2311fd6dfbe85862e9d575561c1f8
nicsiv <nicoleusivek@gmail.com>
2021-02-16 20:46:30 -0600

    Active storage

--- app/models/easel.rb
+++ app/models/easel.rb
@@ -1,3 +1,6 @@
 class Easel < ApplicationRecord
+    has_one_attached :photo
     belongs_to :user, foreign_key: "user_id"
 end
+
+Easel.joins(:photo_attachment).where('published_at >= ?', Time.now)
