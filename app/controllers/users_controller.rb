5d61e210fbf59bdb062f653e6ac26e3efcd49c76
nicsiv <nicoleusivek@gmail.com>
2021-02-16 19:42:54 -0600

    nicole

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: users, include: [:username, :password_digest]
+        render json: users
       end 
 end
