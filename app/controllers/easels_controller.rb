5732388cefa6355a36684f74c4bc57f206c4a7cf
Evan <evanfujita@gmail.com>
2021-02-17 13:34:27 -0600

    easels controller, route for saving image

--- app/controllers/easels_controller.rb
+++ app/controllers/easels_controller.rb
@@ -15,11 +15,20 @@ class EaselsController < ApplicationController
     end 
    end
 
+   def grab_image
+    url = params[:image]
+    downloaded_image = open(url)
+    byebug
+    user_id = params[:user_id]
+    user = User.find(user_id)
+    easels = user.easels.count + 1
+    self.photo.attach(io: downloaded_image, filename: '#{params[user_id]}/#{easels}.jpg')
+end
+
    private
 
    def easel_params
-    params.require(:easel).permit(:name, :user_id, :image, :photo)
+    params.require(:easel).permit(:user_id, :image)
    end
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_020229) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
